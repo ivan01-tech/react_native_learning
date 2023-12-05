ff48d8150ab8cf9ccafcce8483ab56b99ed1e9b5
ivan01-tech <ivansilatsa@gmail.com>
2023-12-05 16:11:57 +0100

    get a little familair with stylesheet

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import {Button, Linking, StatusBar, StyleSheet, Text, View} from 'react-native';
+import StyleSheetApp from './src/StyleSheet';
+// import {Button, Linking, StatusBar, StyleSheet, Text, View} from 'react-native';
 
 function App(): JSX.Element {
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#57446798" />
+      <StyleSheetApp />
+
+      {/* <StatusBar barStyle="light-content" backgroundColor="#57446798" />
       <View>
         <View style={styles.body}>
           <Text>Hello world !</Text>
@@ -15,19 +18,19 @@ function App(): JSX.Element {
             title="Link to the First App"
           />
         </View>
-      </View>
+      </View> */}
     </>
   );
 }
 
-const styles = StyleSheet.create({
-  body: {backgroundColor: '#AEAE', alignItems: 'center'},
-  button: {
-    padding: 10,
-    display: 'flex',
-    justifyContent: 'center',
-    margin: 'auto',
-  },
-});
+// const styles = StyleSheet.create({
+//   body: {backgroundColor: '#AEAE', alignItems: 'center'},
+//   button: {
+//     padding: 10,
+//     display: 'flex',
+//     justifyContent: 'center',
+//     margin: 'auto',
+//   },
+// });
 
 export default App;
---
+++ src/StyleSheet.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
+
+// type Props = {};
+
+const StyleSheetApp = () => {
+  return (
+    <View style={styles.body}>
+      <Text>StyleSheet</Text>
+
+      <Button title="Change the name" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  body: {
+    backgroundColor: '#AEAE',
+    alignItems: 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    borderWidth: 10,
+    gap: 10,
+    borderColor: 'pink',
+
+    height: 150,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'red',
+    width: 500,
+  },
+});
+
+export default StyleSheetApp;
