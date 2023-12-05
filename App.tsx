5290e0cf709e8bf856df2574556fd77dcfd09b57
ivan01-tech <ivansilatsa@gmail.com>
2023-12-05 15:40:52 +0100

    complete with the first video

---
+++ App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {Button, Linking, StatusBar, StyleSheet, Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#57446798" />
+      <View>
+        <View style={styles.body}>
+          <Text>Hello world !</Text>
+        </View>
+        <View style={styles.button}>
+          <Button
+            onPress={() => Linking.openURL('https://youtube.com')}
+            title="Link to the First App"
+          />
+        </View>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  body: {backgroundColor: '#AEAE', alignItems: 'center'},
+  button: {
+    padding: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    margin: 'auto',
+  },
+});
+
+export default App;
